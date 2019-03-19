12780291ec293967dac3d64c6e66456809a7c381
Lukas Schwab <lukas.schwab@gmail.com>
2019-03-18 22:03:20 -0700

    Add threadsafing

--- cork/cork.go
+++ cork/cork.go
@@ -2,20 +2,20 @@ package cork
 
 import (
 	"log"
+	"sync"
 
 	"github.com/fsnotify/fsnotify"
-	"github.com/patrickmn/go-cache"
 )
 
 type Event struct {
-  fsnotify.Event
+	fsnotify.Event
 }
 
 type FileSelector func() []string
 
 type TriggerFilter func(e Event) bool
 
-type ActionIntoCache func(e Event) string
+type ActionIntoCache func(e Event, cached string) string
 
 type ActionGroup struct {
 	Selector FileSelector
@@ -24,56 +24,75 @@ type ActionGroup struct {
 }
 
 type Cork struct {
-	cache *cache.Cache
-  watchers []*fsnotify.Watcher
+  sync.RWMutex
+	cache    map[string]string
+	watchers []*fsnotify.Watcher
 }
 
 func Init() (*Cork, error) {
 	return &Cork{
-		cache: cache.New(cache.NoExpiration, cache.NoExpiration),
-    watchers: make([]*fsnotify.Watcher, 0),
+		cache:    make(map[string]string),
+		watchers: make([]*fsnotify.Watcher, 0),
 	}, nil
 }
 
-func (c *Cork) Close() {
-  for _, w := range c.watchers {
-    log.Println("Destroying a watcher.")
-    w.Close()
-  }
+func (c *Cork) GetCache(key string) string {
+	c.RLock()
+	defer c.RUnlock()
+	if val, ok := c.cache[key]; ok {
+		return val
+	}
+	return ""
 }
 
+func (c *Cork) SetCache(key string, val string) {
+	c.Lock()
+	defer c.Unlock()
+	c.cache[key] = val
+}
+
+func (c *Cork) Close() {
+	for _, w := range c.watchers {
+		log.Println("Destroying a watcher.")
+		w.Close()
+	}
+}
+
+// TODO: rerun selectors to find new files. Alternatively, depend on filter:
+// watch all of the files in the cwd by default.
+// TODO: should the cache exist on a group level or on a global level?
+// Probably on a group level...
 func (c *Cork) Add(ag ActionGroup) error {
-  w, err := fsnotify.NewWatcher()
-  if err != nil {
-    return err
-  }
-  c.watchers = append(c.watchers, w)
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	c.watchers = append(c.watchers, w)
 
-  go func() {
-    for {
-      select {
-      case event, ok := <-w.Events:
-        if !ok {
-          log.Println("There was an error in an event consumer [events].")
-          return
-        }
-        e := Event{event}
-        if ag.Filter(e) {
-          log.Println("Filter true.")
-          ag.Action(e)
-        } else {
-          log.Println("Filter false.")
-        }
-      case err, ok := <- w.Errors:
-        if !ok {
-          log.Println("There was an error in an event consumer [errs].")
-        }
-        log.Println("error:", err)
-      }
-    }
-  }()
+	go func() {
+		for {
+			select {
+			case event, ok := <-w.Events:
+				if !ok {
+					log.Println("There was an error in an event consumer [events].")
+					return
+				}
+				e := Event{event}
+				if ag.Filter(e) {
+					newVal := ag.Action(e, c.GetCache(e.Name))
+					c.SetCache(e.Name, newVal)
+				}
+			case err, ok := <-w.Errors:
+				if !ok {
+					log.Println("There was an error in an event consumer [errs].")
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
 
-  err = w.Add(ag.Selector()[0])
+	err = w.Add(ag.Selector()[0])
 
-  return nil
+	return nil
 }
--- main.go
+++ main.go
@@ -1,31 +1,44 @@
 package main
 
 import (
-  "log"
-  "time"
+	"log"
+	"time"
 
 	"./cork"
 )
 
 func main() {
 	c, _ := cork.Init()
-  defer c.Close()
+	defer c.Close()
 
-  ag := cork.ActionGroup{
-    Selector: func() []string {
-      log.Println("From MAIN: selector running.")
-      return []string{"./testdir",}
-    },
-    Filter: func(e cork.Event) bool {
-      log.Println("From MAIN: filter running.")
-      return true
-    },
-    Action: func(e cork.Event) string {
-      log.Println("From MAIN: action running.")
-      return "Cache this, boys!"
-    },
-  }
-  c.Add(ag)
+	ag := cork.ActionGroup{
+		Selector: func() []string {
+			log.Println("From MAIN: selector running.")
+			return []string{"./testdir"}
+		},
+		Filter: func(e cork.Event) bool {
+			return true
+		},
+		Action: func(e cork.Event, cached string) string {
+			return "Cache this, boys!"
+		},
+	}
+	c.Add(ag)
 
-  time.Sleep(300 * time.Second)
+	two := cork.ActionGroup{
+		Selector: func() []string {
+			log.Println("From MAIN: special selector running.")
+			return []string{"./testdir/special"}
+		},
+		Filter: func(e cork.Event) bool {
+			return true
+		},
+		Action: func(e cork.Event, cached string) string {
+			return "Special cache."
+		},
+	}
+	c.Add(two)
+
+  // FIXME: run indefinitely.
+	time.Sleep(300 * time.Second)
 }
