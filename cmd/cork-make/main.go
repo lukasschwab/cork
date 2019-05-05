d5f90077f552b5a7bf372edccf5970f8ad503c1f
Lukas Schwab <lukas.schwab@gmail.com>
2019-05-05 13:24:46 -0700

    Add partially working cork-make

--- cmd/cork-make/main.go
+++ cmd/cork-make/main.go
@@ -1,43 +1,83 @@
 package main
 
 import (
-  "log"
-  "os"
-  "strings"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
 
-  // "github.com/lukasschwab/cork"
+	"github.com/lukasschwab/cork"
 )
 
 var stderr = log.New(os.Stderr, "", 0)
 var stdout = log.New(os.Stderr, "", 0)
 
+var allWatchers = make([]*cork.Watcher, 0)
+
 func main() {
-  parsePatterns(os.Args[1:])
+	defer cleanup()
+	parsePatterns(os.Args[1:])
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+	select {
+	case <-c:
+		break
+	}
+}
+
+func cleanup() {
+	stdout.Println("Cleaning up.")
+	for _, w := range allWatchers {
+		w.Close()
+	}
 }
 
 func parsePatterns(args []string) {
-  if len(args) == 0 {
-    return
-  }
-  if args[0] != "-p" && args[0] != "--pattern" {
-    stderr.Println("First argument must be a pattern.")
-    os.Exit(2)
-  }
-  var i int;
-  for i = 1; i < len(args) && args[i][0] != '-'; i++ {}
-  parseCommand(args[1:i], args[i:])
+	if len(args) == 0 {
+		return
+	}
+	if args[0] != "-p" && args[0] != "--pattern" {
+		stderr.Println("First argument must be a pattern.")
+		os.Exit(2)
+	}
+	var i int
+	for i = 1; i < len(args) && args[i][0] != '-'; i++ {
+	}
+	parseCommand(args[1:i], args[i:])
 }
 
 func parseCommand(patterns []string, args []string) {
-  if len(args) < 2 || (args[0] != "-r" && args[0] != "--run") {
-    stderr.Println("Patterns must be followed by a -r or --run.")
-    os.Exit(2)
-  }
-  command := args[1]
-  stdout.Printf("['%s'] → %s", strings.Join(patterns, "', '"), command)
-  parsePatterns(args[2:])
+	if len(args) < 2 || (args[0] != "-r" && args[0] != "--run") {
+		stderr.Println("Patterns must be followed by a -r or --run.")
+		os.Exit(2)
+	}
+	watch(patterns, args[1])
+	parsePatterns(args[2:])
 }
 
-func watch(patterns []string, cmd string) {
-  // TODO: set up watcher.
+func watch(patterns []string, cmdString string) {
+	stdout.Printf("Watching ['%s'] → %s", strings.Join(patterns, "', '"), cmdString)
+	w, err := cork.Watch(cork.SelectPatterns(patterns), runCmd(cmdString).OnFileChange())
+	if err != nil {
+		stderr.Println("Error creating watcher:", err)
+		return
+	}
+	allWatchers = append(allWatchers, w)
+}
+
+func runCmd(cmdString string) cork.Action {
+	splitCmd := strings.Split(cmdString, " ") // FIXME: breaks on spaces in command args.
+	return func(e cork.Event, cached string) string {
+		stdout.Printf("%s → %s", e.Name, cmdString)
+		out, err := exec.Command(splitCmd[0], splitCmd[1:]...).Output()
+		if err != nil {
+			stderr.Println("Error:", err)
+		}
+		if out != nil {
+			stdout.Println("Output:", string(out))
+		}
+		return "" // Discarded for hash.
+	}
 }
