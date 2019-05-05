5ba81de35c486138ef16a3b91d8f7e19c9379f7b
Lukas Schwab <lukas.schwab@gmail.com>
2019-05-05 12:20:12 -0700

    Add cork-make argument parsing. Needs testing.

--- cmd/cork-make/README.md
+++ cmd/cork-make/README.md
@@ -1,3 +1,1 @@
-# cork/cork-make
-
-`cork-make` aims to offer an interface analagous to `watchman-make`.
+# cork-make
--- cmd/cork-make/main.go
+++ cmd/cork-make/main.go
@@ -1,3 +1,43 @@
 package main
 
-func main() {}
+import (
+  "log"
+  "os"
+  "strings"
+
+  // "github.com/lukasschwab/cork"
+)
+
+var stderr = log.New(os.Stderr, "", 0)
+var stdout = log.New(os.Stderr, "", 0)
+
+func main() {
+  parsePatterns(os.Args[1:])
+}
+
+func parsePatterns(args []string) {
+  if len(args) == 0 {
+    return
+  }
+  if args[0] != "-p" && args[0] != "--pattern" {
+    stderr.Println("First argument must be a pattern.")
+    os.Exit(2)
+  }
+  var i int;
+  for i = 1; i < len(args) && args[i][0] != '-'; i++ {}
+  parseCommand(args[1:i], args[i:])
+}
+
+func parseCommand(patterns []string, args []string) {
+  if len(args) < 2 || (args[0] != "-r" && args[0] != "--run") {
+    stderr.Println("Patterns must be followed by a -r or --run.")
+    os.Exit(2)
+  }
+  command := args[1]
+  stdout.Printf("['%s'] → %s", strings.Join(patterns, "', '"), command)
+  parsePatterns(args[2:])
+}
+
+func watch(patterns []string, cmd string) {
+  // TODO: set up watcher.
+}
